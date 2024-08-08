1e4aa450398fe18a04a3dd2ca8493de88b84a862
Edwina Dsouza <115996056+edwinadsouza@users.noreply.github.com>
2024-08-08 22:35:34 +0530

    Time: 138 ms (91.84%), Space: 13.3 MB (72.45%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def winnerOfGame(self, colors):
+        '''
         n = len(colors)
         alice = 0
         bob = 0
@@ -10,4 +11,22 @@ class Solution(object):
                 else:
                     bob +=1
         return alice > bob
+'''
+        alice = 0
+        bob = 0
+        consecutive_A = 0
+        consecutive_B = 0
+
+        for color in colors:
+            if color == 'A':
+                consecutive_A += 1
+                consecutive_B = 0
+                if consecutive_A >= 3:
+                    alice += 1
+            elif color == 'B':
+                consecutive_B += 1
+                consecutive_A = 0
+                if consecutive_B >= 3:
+                    bob += 1
         
+        return alice > bob
