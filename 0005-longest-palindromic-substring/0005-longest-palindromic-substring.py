03344b80cc29878481cd0001d691885cb314b764
Edwina Dsouza <115996056+edwinadsouza@users.noreply.github.com>
2024-08-10 00:17:55 +0530

    Time: 327 ms (84.87%), Space: 11.8 MB (62.55%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,4 +1,4 @@
-class Solution(object):
+'''class Solution(object):
     def longestPalindrome(self, s):
         def centre(s,l,r):
             max_len = 0
@@ -19,4 +19,18 @@ class Solution(object):
                 result = odd
             if len(even)>len(result):
                 result = even
+        return result
+        '''
+class Solution(object):
+    def longestPalindrome(self, s):
+        def center(s, l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return s[l+1:r]  
+        result = ""
+        for i in range(len(s)): 
+            odd = center(s, i, i)            
+            even = center(s, i, i + 1)           
+            result = max(result, odd, even, key=len)        
         return result
